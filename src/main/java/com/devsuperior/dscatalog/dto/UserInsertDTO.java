436c06c18b606afe1dfe7f89be3fc05fd623896e
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-02-14 02:23:14 -0300

    custom constraint validator

--- src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dscatalog.dto;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
     private String password;
